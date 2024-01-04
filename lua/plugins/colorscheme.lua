7d3f425678046a602995d498ec3689859042a640
Ismael Marquez <imarquez99@gmail.com>
2024-01-04 00:52:07 -0300

    Add catpuccin theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = { flavour = "macchiato" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
